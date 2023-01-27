7f12bdfdaa17190851dc3799eda3ed19f5ba5fe5
Martin Todorov <106689924+Martin3255@users.noreply.github.com>
2023-01-27 17:11:05 +0200

    Add files via upload
    More practice

---
+++ 04. Even Times/05. Count Symbols/Count Symbols .cs
@@ -0,0 +1,14 @@
+﻿SortedDictionary<char, int> countOfChars = new SortedDictionary<char, int>();
+string line = Console.ReadLine();
+for (int i = 0; i < line.Length; i++)
+{
+	if (!countOfChars.ContainsKey(line[i]))
+	{
+		countOfChars.Add(line[i], 0);
+	}
+	countOfChars[line[i]]++;
+}
+foreach (var pair in countOfChars)
+{
+	Console.WriteLine($"{pair.Key}: {pair.Value} time/s");
+}
